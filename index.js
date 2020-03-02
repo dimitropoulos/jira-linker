2fc6600241b9b58adedc454c84bd79c2ca167c6a
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2020-03-02 10:22:07 -0500

    adds link to the script in... in the... in the script
    for consistency, woop woop!

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ This is a GitHub extension that will automatically link back to your private JIR
 
 1. install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
 1. create a new script via the extension once it's installed
-1. copy/paste this code to your new script
+1. copy/paste [this code](https://raw.githubusercontent.com/dimitropoulos/jira-linker/master/index.js) to your new script
 1. save the script
 1. refresh github
 1. follow the form in the lower left hand part of the screen
